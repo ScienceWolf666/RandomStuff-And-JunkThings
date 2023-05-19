76283ee355abcb0438ca430732e8fe002f172fba
Science_Wolf <79545263+ScienceWolf666@users.noreply.github.com>
2023-05-19 04:12:07 +0200

    Add files via upload

---
+++ TestPortal-Cheats Chrome Extension/README.md
@@ -0,0 +1,10 @@
+# TestPortalCheats
+TestPortalCheats-EXPLOIT
+---------------------------
+
+HOW TU USE:
+
+1. Open settings in Chrome
+2. Click Extensions
+3. Click Developer Mode
+4. Click add unpacked
---
+++ TestPortal-Cheats Chrome Extension/TestPortalExploit/pageScript.js
@@ -0,0 +1,18 @@
+try{
+
+	var original = RegExp.prototype.test;
+RegExp.prototype.test = function (s) {
+	if (this.toString().includes("native code") && this.toString().includes("function")) {
+		//all is fine man just continue
+		return true;
+	}
+	
+	var r = original.call(this, s);
+	return r;
+};
+document.hasFocus = function () {return true;};
+
+	window.postMessage({action: 'GOT_DUCK', payload: true}, '*');
+}catch(error){
+	window.postMessage({action: 'GOT_DUCK', payload: false}, '*');
+}
